5430f11d27ca72249e39f9c073b6aaa1e140e57b
28704 Jonathon Sonesen <jonathon.sonesen@syccuxas01.pcc.edu>
2015-04-19 14:29:19 -0700

    finished CS201/Asignent1p

--- CS201/asgmt01/asgmt01/a01p.c
+++ CS201/asgmt01/asgmt01/a01p.c
@@ -8,22 +8,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static char *studentName = "Jonathon Sonesen";
+static char *studentName = "I. Forgot";
 
 // report whether machine is big or small endian
 void bigOrSmallEndian()
 {
     unsigned int checkEnd = 1;
-    char *endChar=(char*)&checkEnd;
-   if(*endChar == 1)
-   {
-       printf("byte order: little endian\n\n");
-   }
-   else
-   {
-       printf("byte order: big endian\n\n");
+    char *endChar         = ( char*) & checkEnd;
 
-   }
+    if(*endChar == 1)
+        printf("byte order: little-endian\n\n");
+    else
+        printf("byte order: big-endian \n\n");
 }
 
 // get next int (entered in hex) using scanf()
@@ -31,28 +27,89 @@ void bigOrSmallEndian()
 // if call succeeded, return int value via i pointer
 int getNextHexInt(unsigned int *i)
 {
-	int ret = 0;
-        if(scanf("%x", i) == 1)
-            ret = 1;
-        else
-            ret = 0;
-	return ret;
+    int ret = 0;
+    if(scanf("%x", i) == 1)
+        ret = 1;
+    else 
+        ret = 0;
+
+    return ret;
 }
 
 // print requested data for the given number
 void printNumberData(int i)
 {
-    int bitSign = 0;
+    //check the sign bit
+    int signBit = 0;
     if((i & 0x80000000) == 0x80000000)
     {
-        printf("bitSign 1, "); //denotes a negative num
-        bitSign = 1;
+        printf("signBit 1, "); //neg
+        signBit = 1;
     }
     else
     {
-        printf("bitSign 0, "); //denotes a psitive num
-        bitSign = 0;
+        printf("signBit 0, "); //pos
+        signBit = 0;
     }
+
+    //bit control flow var
+    int expBits  = 0;
+    int fracBits = 0;
+    int norm     = 1;
+
+    //mask exp and frac bits
+    expBits  = i & 0x7F800000; //mask sign and frac
+    expBits  = expBits >> 23; //shift bits to right
+    fracBits = i & 0x007FFFFF; //mask frac
+    fracBits = i & 0x007FFFFF; //mask frac
+
+    //output info
+    printf("expBits %3d, fractBits 0x%08X\n", expBits, fracBits);
+    
+    //check norm
+    if(expBits == 0)
+    {
+        if(fracBits != 0)
+        {
+            norm = 0;
+            printf("denormalized: exp = %d", (expBits - 126));
+        }
+        else
+        {
+            norm = 0;
+            if(signBit == 0)
+            {
+                printf("+zero");
+            }
+            else
+            {
+                printf("-zero");
+            }
+        }
+
+    }
+    else
+    {
+        if (expBits == 0xff)
+        {
+            if(fracBits == 0)
+            {
+                printf("+/- Infinity");
+            }
+            else
+            {
+                printf("%cNan\n\n", fracBits < 0x400000 ? 'S' : 'Q');
+            }
+        }
+
+        if(expBits != 0 && expBits != 0xff)
+        {
+            norm = 1;
+            printf("normalize: exp = %d", (expBits - 127));
+        }
+    }
+    printf("\n\n");
+
 
 }
 
