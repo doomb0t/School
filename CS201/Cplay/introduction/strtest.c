c5975c6ef8ffdaa1d9acca5814e2954ecbfacab8
JonathonSonesen <jonathon.sonesen@pcc.edu>
2015-05-16 20:49:34 -0700

    remove cPlay folder

--- CS201/Cplay/introduction/strtest.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main (int argc, char **argv)
-{
-    char    stra[] = "abcdefghijkl";
-    char    *strp =  "zyxwvutsrqpo";
-
-    return 0;               // indicate successful run to Unix
-}
