464486291f49bff6a220c9349dabb224f7e9db3c
JonathonSonesen <jonathon.sonesen@pcc.edu>
2015-05-17 05:21:05 -0700

    improves comments format

--- CS201/asgmt02/a02p.c
+++ CS201/asgmt02/a02p.c
@@ -31,13 +31,13 @@ double quadraticRoot(double a, double b, double c)
                 "fsqrt                    \n"  /* sqrt(b^2-4ac)          */
                 "fldl   %[b]              \n"  /* b                      */
                 "fchs                     \n"  /* change sign-b          */
-                "faddp  %%ST(1)           \n"  /* +sqrt(b*b-4ac)         */
+                "faddp  %%ST(1)           \n"  /* -b + sqrt(b*b-4ac)     */
                 "fdivp  %%ST(1)           \n"  /* -b + sqrt(b^2-4ac/2    */
-                :"=t"(root)                    /* out,I  used '=t' to return top of 80387             */
-                                                 /* floating point stack which is where the result is */
-                                                 /* Source: gcc online machine constraint docs        */ 
-                :[a]"m"(a), [b]"m"(b), [c]"m"(c) /* in                   */
-                :"eax"                           /* clobber              */
+                :"=t"(root)                    /* out,I  used '=t' to return top of floating */
+                                               /* point stack which is where the result is   */
+                                               /* Source: gcc online machine constraint docs */ 
+                :[a]"m"(a),[b]"m"(b),[c]"m"(c) /* in                     */
+                :"eax"                         /* clobber                */
 	);
 
 	return root;
