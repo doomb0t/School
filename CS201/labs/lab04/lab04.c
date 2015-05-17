71924d39e785e611b9a2aaa6b1e8dc58e8755932
JonathonSonesen <jonathon.sonesen@pcc.edu>
2015-05-16 23:28:49 -0700

    adds skeleton for lab4

---
+++ CS201/labs/lab04/lab04.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lab04.c
+ *
+ *    Description:  A program that reads in hex integer value and masks bits 4-3
+ *                  then shifts them to the right to use them as numbers
+ *                  then the program prints their sum
+ *
+ *        Version:  1.0
+ *        Created:  05/16/15 23:22:25
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jonathon Sonesen
+ *   Organization:  PCC CS201 Computer Systems
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+
+}
