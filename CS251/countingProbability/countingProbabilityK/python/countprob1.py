5cde9a8a14ea8a6a51df78b1cb56d0a1d9168b84
JonathonSonesen <jonathon.sonesen@pcc.edu>
2015-05-20 15:38:00 -0700

    fixes data type and syntax errors

--- CS251/countingProbability/countingProbabilityK/python/countprob1.py
+++ CS251/countingProbability/countingProbabilityK/python/countprob1.py
@@ -1,6 +1,8 @@
-from itertool import chain, combinations
+from itertools import chain, combinations
+players = ['a','b']
+
 def all_subsets(ss):
     return chain(*map(lambda x: combinations(ss, x), range(0, len(ss)+1)))
 
-for subset in all_subsets(stuff):
+for subset in all_subsets(players):
     print(subset)
