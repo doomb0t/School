433b976ed6d055ba2d2682d34ec1313c476b64d0
JonathonSonesen <jonathon.sonesen@pcc.edu>
2015-05-25 17:33:43 -0700

    lab04 done

--- CS201/labs/lab04/lab04.c
+++ CS201/labs/lab04/lab04.c
@@ -3,7 +3,7 @@
  *
  *       Filename:  lab04.c
  *
- *    Description:  A program that reads in hex integer value and masks bits 4-3
+ *    Description:  A program that reads in hex1 integer value and masks bits 4-3
  *                  then shifts them to the right to use them as numbers
  *                  then the program prints their sum
  *
@@ -23,19 +23,19 @@
 
 int main (int argc, char **argv)
 {
-    int   hex = 0;
+    int   hex1 = 0;
     int   sum = 0;
     char  trailing = '\0';
-    sscanf(argv[1], "%x, %s\n", &hex, &trailing);
-    char * p =(char *) &hex;
-    printf("You entered: %d\n", hex);
+    sscanf(argv[1], "%x, %s\n", &hex1, &trailing);
+    char * p =(char *) &hex1;
+    printf("You entered: %d\n", hex1);
     
     for(int i = 0; i < 4; i++)
     {
         sum +=( p[i] & 0x18) >> 3;
     
     }
-    
-    printf("Final: %d, %d \n", hex, sum);
+ 
+    printf("Final: %d, %d \n", hex1, sum);
     return 0;
 }
