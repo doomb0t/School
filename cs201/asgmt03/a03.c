6241b568d3c2fc56307b5ccb210dd020069e529c
JonathonSonesen <jonathon.sonesen@pcc.edu>
2015-05-29 14:36:41 -0700

    changes to a03.c

--- cs201/asgmt03/a03.c
+++ cs201/asgmt03/a03.c
@@ -1,3 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  a03.c
+ *
+ *    Description:  reads argv char one at a time from child to parent
+ *                  outputs how many char's  the proc counts through pipe
+ *
+ *        Version:  1.0
+ *        Created:  05/29/2015 01:42:47 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jonathon Sonesen
+ *         Source:  starter code by Michael Trigiboff PhD.
+ *   Organization:  PCC CS201 Computer Systems
+ *
+ * =====================================================================================
+ */
+
 // Characters from command line arguments are sent to child process
 // from parent process one at a time through pipe.
 //
@@ -6,15 +26,36 @@
 // Child process returns number of characters counted to parent process.
 //
 // Parent process prints number of characters counted by child process.
-void read_from_pipe (char** in)
-{
 
-}
 #include <stdio.h>
 #include <sys/wait.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+
+/*ERROR HANDLING*/
+#define READ_ERR        -2
+#define WRITE_ERR       -3
+#define PROTOCOL_ERR    -1
+
+/*PROTOCOL CMD DEF*/
+#define OPEN_CMD        -12
+#define ACK_CMD         -10
+#define CLOSE_CMD       -13
+
+static int fd;
+
+void read_from_pipe (char** in)
+{
+    char ch;
+    if(read(fd, &in, 1) == 1)
+        return;
+    else {
+        printf("readFromPipe: read err\n");
+        }
+}
+
+
 
 int main(int argc, char **argv)
 {
