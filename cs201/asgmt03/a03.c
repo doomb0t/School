ab0dd3e5b227280c798f1765f046264fbbb6b582
JonathonSonesen <jonathon.sonesen@pcc.edu>
2015-05-29 17:59:42 -0700

    almost done with asmgt03

--- cs201/asgmt03/a03.c
+++ cs201/asgmt03/a03.c
@@ -43,80 +43,119 @@
 #define ACK_CMD         -10
 #define CLOSE_CMD       -13
 
-static int fdIn, fdOut;
+static int fd_in, fd_out;
 static int to_child[2];
 static int from_child[2];
 
-void read_from_pipe (char** in)
+char read_pipe ()
 {
     char c;
 
-    if(read(fdIn, &c, 1) == 1) {
-        in = (char**)&c;
+    if(read(fd_in, &c, 1) == 1) {
+        return c;
         }
     else {
-        printf("read_from_pipe: read err\n");
+        printf("read_pipe: read err\n");
         exit(WRITE_ERR);
         }
 }
 
+void write_pipe (char c)
+{
+    if(write(fd_out, &c, 1) != 1) {
+            printf("write_pipe: err\n");
+            exit(WRITE_ERR);
+            }   
+}
 
+void get_cmd (char cmd)
+{
+    char c = read_pipe();
+
+    if(c == cmd)
+        write_pipe(ACK_CMD);
+    else {
+        printf("get_cmd: protocol err\n");
+        exit(PROTOCOL_ERR);
+        }
+}
+
+void send_cmd (char cmd)
+{
+    char c;
+    write_pipe(cmd);
+    if((c =  read_pipe()) != ACK_CMD) {
+        printf("send_cmd: protocol err, read '%d' 0x%X\n", c, c & 0xFF);
+        exit(PROTOCOL_ERR);
+        }
+}
+
+char get_data (char cmd)
+{
+    char c = read_pipe();
+    write_pipe(ACK_CMD);
+    return c;
+}
+
+void send_data (char cmd)
+{
+    char c;
+    write_pipe(cmd);
+    if((c = read_pipe()) != ACK_CMD) {
+        printf("send_data: err\n");
+        exit(PROTOCOL_ERR);
+        }
+    printf("parents: '%c' acknowledged\n", c);
+}
 int main(int argc, char **argv)
 {
-        pid_t pid;
-        char c;
-        int i;
+    pid_t pid;
+    char c;
+    int i;
 	
-        // set up pipe to child
-        if(pipe(to_child)) {
-            printf("pipe: to child: err\n");
-            return -1;
-            }  
+    // set up pipe to child
+    if(pipe(to_child)) {
+        printf("pipe: to child: err\n");
+        return -1;
+        }  
 
-        //pipe from child
-        if(pipe(from_child)) {
-            printf("pipe from child err\n");
-            return -1;
-            }
+    //pipe from child
+    if(pipe(from_child)) {
+        printf("pipe from child err\n");
+        return -1;
+        }
         
-	// create parent and child 
-        pid = fork();
+    // create parent and child 
+    pid = fork();
         
-        //error handling
-        if(pid < 0) {
-            printf("fork err%d\n", pid);
-            }
-	// -- running in child process --
-        else if (pid == 0) {
-	    int     nChars = 0;
-            close(from_child[0]);
-            fdOut = from_child[1];
-            fdIn = to_child[0];
-            close(to_child[1]);
+    //error handling
+    if(pid < 0) {
+        printf("fork err%d\n", pid);
+        }
+    // -- running in child process --
+    else if (pid == 0) {
+	int     nChars = 0;
+        close(from_child[0]);
+        fd_out = from_child[1];
+        fd_in = to_child[0];
+        close(to_child[1]);
+                        
+        while(read_pipe() != '\0' && read_pipe() != '\n')
+            nChars++;
+        return nChars;
+	// Receive characters from parent process via pipe
+	// one at a time, and count them.
             
-            
-	    // Receive characters from parent process via pipe
-	    // one at a time, and count them.
+	// Return number of characters counted to parent process.
+	return nChars;
+	}
+    else {
+	// -- running in parent process --
+	int     nChars = 0;
 
-	    // Return number of characters counted to parent process.
-	    return nChars;
-	    }
-	else {
-	    // -- running in parent process --
-	    int     nChars = 0;
+	printf("CS201 - Assignment 3 - Jonathon Sonesen\n");
 
-	    printf("CS201 - Assignment 3 - Jonathon Sonesen\n");
-
-	    // Send characters from command line arguments starting with
-	    // argv[1] one at a time through pipe to child process.
-
-	    // Wait for child process to return. Reap child process.
-	    // Receive number of characters counted via the value
-	    // returned when the child process is reaped.
-
-	    printf("child counted %d characters\n", nChars);
-	    return 0;
-	    }
-
-        
+	printf("child counted %d characters\n", nChars);
+	return 0;
+	} 
 }
