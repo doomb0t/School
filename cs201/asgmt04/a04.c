7ab7118c720e0e86472e179555bca30ffeacc3cb
JonathonSonesen <jonathon.sonesen@pcc.edu>
2015-06-03 18:14:54 -0700

    fixes a04

--- cs201/asgmt04/a04.c
+++ cs201/asgmt04/a04.c
@@ -10,11 +10,16 @@ int main(void)
 {
  
     double  *array = calloc(ARRAY_SIZE, sizeof(double));
-    register double  sum = 0;          //using register double GREATLY decreases execution time
+    double  sum = 0;          //using register double GREATLY decreases execution time
                                        //not sure if I am allowed to do this though
     int     i;
     // You can add variables between this comment ...
-    register double* j; 
+    double* j;
+    double sum0 = 0;
+    double sum1 = 0;
+    double sum2 = 0;
+    double sum3 = 0;
+    double sum4 = 0;
     
     // ... and this one.
     // Please change 'your name' to your actual name.
@@ -23,18 +28,22 @@ int main(void)
     for (i = 0; i < N_TIMES; i++) {
         
         j = array + (ARRAY_SIZE);     
-        while(array < j - 1) {      
-            sum += *j + *(j - 1);
-            j-=2;
+        while(array < j - 15) {      
+            sum0 += *j + *(j - 1) + *(j -2)+ *(j -3);
+            sum1 += *(j - 4) + *(j -5)+ *(j -6);
+            sum2 += *(j - 7) + *(j -8)+ *(j -9);
+            sum3 += *(j - 10) + *(j -11)+ *(j - 12);
+            sum4 += *(j - 13) + *(j -14);
+            j-=15;
             }
        
         if (j > array) {
-            sum += *j;
+            sum0 += *j;
             }
     }
     // You can add some final code between this comment ...
-    
-    
+    sum += (sum0 + sum1 + sum2 + sum3 + sum4);
+     
     // ... and this one.
     return 0;
 }
